13edae4e82b16df09caed82d9113a95c37d6a9e6
dilreet-dhaliwal <119907628+dilreet-dhaliwal@users.noreply.github.com>
2023-03-20 21:14:59 -0400

    Add files via upload

---
+++ C82main.js
@@ -0,0 +1,28 @@
+
+canvas= document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+
+canvas.addEventListener("mousedown", my_mousedown);
+function my_mousedown(e)
+{
+  color= document.getElementById("color").value;
+  width_of_line= document.getElementById("width_of_line").value;
+   radius= document.getElementById("radius").value;
+  mouseEvent="mouseDown";
+}
+function my_mousemove(e)
+{
+  current_position_of_mouse_x=e.clientX - canvas.OffsetLeft;
+  current_position_of_mouse_y=e.clientY - canvas.OffsetTop;
+  
+  if (mouseEvent=="mousedown"){
+    console.log("current position of x and y coordinates =");
+    console.log(" x ="+ current_position_of_mouse_x +" y ="+ current_position_of_mouse_y);
+    ctx.beginPath();
+    ctx.strokeStyle=color;
+    ctx.lineWidth= width_of_line;
+    ctx.arc(current_position_of_mouse_x, current_position_of_mouse_y, radius, 0, 2*Math.PI);
+    ctx.stroke();
+  }
+  
+}
